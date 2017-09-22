cd271b8b73e3040415ad7af064b76340ef5fe8c2
Shunichi Shinohara <shino@shiguredo.jp>
2017-09-22 16:40:31 +0900

    media stream スキップは自分が upstream のときに限る

--- sora-android-sdk/src/main/java/jp/shiguredo/sora/sdk/channel/SoraMediaChannel.kt
+++ sora-android-sdk/src/main/java/jp/shiguredo/sora/sdk/channel/SoraMediaChannel.kt
@@ -114,7 +114,7 @@ class SoraMediaChannel(
 
         override fun onAddRemoteStream(ms: MediaStream) {
             SoraLogger.d(TAG, "[channel:$role] @peer:onAddRemoteStream:${ms.label()}")
-            if (clientId != null && ms.label() == clientId) {
+            if (mediaOption.upstreamIsRequired && clientId != null && ms.label() == clientId) {
                 SoraLogger.d(TAG, "[channel:$role] this stream is mine, ignore")
                 return
             }
