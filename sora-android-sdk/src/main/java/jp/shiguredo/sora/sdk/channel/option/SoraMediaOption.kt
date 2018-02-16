c0ebde0f17e8a40400855e59b0ef94d7a0b419a3
Shunichi Shinohara <shino@shiguredo.jp>
2018-02-16 12:16:28 +0900

    メディアオプション kdoc

--- sora-android-sdk/src/main/java/jp/shiguredo/sora/sdk/channel/option/SoraMediaOption.kt
+++ sora-android-sdk/src/main/java/jp/shiguredo/sora/sdk/channel/option/SoraMediaOption.kt
@@ -3,6 +3,9 @@ package jp.shiguredo.sora.sdk.channel.option
 import org.webrtc.EglBase
 import org.webrtc.VideoCapturer
 
+/**
+ * Sora への接続オプションを表すクラスです
+ */
 class SoraMediaOption {
 
     val TAG = SoraMediaOption::class.simpleName
@@ -24,23 +27,59 @@ class SoraMediaOption {
 
     var videoBitrate: Int? = null
 
+    /**
+     * 音声の視聴を有効にします
+     */
     fun enableAudioDownstream() {
         audioDownstreamEnabled = true
     }
 
+    /**
+     * 音声の配信を有効にします
+     */
     fun enableAudioUpstream() {
         audioUpstreamEnabled = true
     }
 
+    /**
+     * マルチストリームを有効にします
+     *
+     * cf.
+     * - Sora ドキュメントのマルチストリーム
+     *   [](https://sora.shiguredo.jp/doc/MULTISTREAM.html)
+     */
     fun enableMultistream() {
         multistreamEnabled = true
     }
 
+    /**
+     * 映像の視聴を有効にします
+     *
+     * cf.
+     * - `org.webrtc.EglBase`
+     * - `org.webrtc.EglBase.Context`
+     *
+     * @param eglContext Egl コンテキスト
+     */
     fun enableVideoDownstream(eglContext: EglBase.Context?) {
         videoDownstreamEnabled = true
         videoDownstreamContext = eglContext
     }
 
+    /**
+     * 映像の配信を有効にします
+     *
+     * cf.
+     * - `org.webrtc.VideoCapturer`
+     * - `org.webrtc.EglBase`
+     * - `org.webrtc.EglBase.Context`
+     * - Sora ドキュメントのスナップショット機能
+     *   [](https://sora.shiguredo.jp/doc/SNAPSHOT.html)
+     *
+     * @param capturer `VideoCapturer` インスタンス
+     * @param eglContext Egl コンテキスト
+     * @param snapshotEnabled スナップショットを用いるか否か
+     */
     fun enableVideoUpstream(capturer:        VideoCapturer,
                             eglContext:      EglBase.Context?,
                             snapshotEnabled: Boolean = false) {
