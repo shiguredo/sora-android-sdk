660d6a3548c763d36767035de2f89279efc54635
Shunichi Shinohara <shino@shiguredo.jp>
2018-02-16 12:35:31 +0900

    option enum まわり kdoc

--- sora-android-sdk/src/main/java/jp/shiguredo/sora/sdk/channel/option/SoraAudioOption.kt
+++ sora-android-sdk/src/main/java/jp/shiguredo/sora/sdk/channel/option/SoraAudioOption.kt
@@ -1,10 +1,17 @@
 package jp.shiguredo.sora.sdk.channel.option
 
+/**
+ * 音声に関するオプションをまとめるクラスです
+ */
 class SoraAudioOption {
 
+    /**
+     * 利用できる音声コーデックを示します。
+     */
     enum class Codec {
+        /** Opus */
         OPUS,
+        /** PCMU */
         PCMU
     }
 }
-
--- sora-android-sdk/src/main/java/jp/shiguredo/sora/sdk/channel/option/SoraVideoOption.kt
+++ sora-android-sdk/src/main/java/jp/shiguredo/sora/sdk/channel/option/SoraVideoOption.kt
@@ -2,41 +2,73 @@ package jp.shiguredo.sora.sdk.channel.option
 
 import android.graphics.Point
 
+/**
+ * 映像に関するオプションをまとめるクラスです
+ */
 class SoraVideoOption {
 
+    /**
+     * 利用できる映像コーデックを示します
+     */
     enum class Codec {
+        /** H.264 */
         H264,
+        /** VP8 */
         VP8,
+        /** VP9 */
         VP9
     }
 
+    /**
+     * 映像のフレームサイズをまとめるクラスです
+     */
     class FrameSize {
 
+        /**
+         * ランドスケープモードで利用できるフレームサイズを示します
+         */
         class Landscape {
 
             companion object {
 
+                /** QQVGA 160x120 */
                 val QQVGA = Point(160, 120)
+                /** QCIF  176x144 */
                 val QCIF  = Point(176, 144)
+                /** HQVGA 240x160 */
                 val HQVGA = Point(240, 160)
+                /** QVGA  320x240 */
                 val QVGA  = Point(320, 240)
+                /** VGA   640x480 */
                 val VGA   = Point(640, 480)
+                /** HD    1280x720 */
                 val HD    = Point(1280, 720)
+                /** FHD   1920x1080 */
                 val FHD   = Point(1920, 1080)
 
             }
         }
 
+        /**
+         * ポートレートモードで利用できるフレームサイズを示します
+         */
         class Portrait {
 
             companion object {
 
+                /** QQVGA 120x160 */
                 val QQVGA = Point(120, 160)
+                /** QCIF  144x176 */
                 val QCIF  = Point(144, 176)
+                /** HQVGA 160x240 */
                 val HQVGA = Point(160, 240)
+                /** QVGA  240x320 */
                 val QVGA  = Point(240, 320)
+                /** VGA   480x640 */
                 val VGA   = Point(480, 640)
+                /** HD    720x1280 */
                 val HD    = Point(720, 1280)
+                /** FHD   1080x1920 */
                 val FHD   = Point(1080, 1920)
 
             }
