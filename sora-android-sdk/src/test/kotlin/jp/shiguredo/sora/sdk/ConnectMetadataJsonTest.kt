1723f3d8c66556b61a83a6e1b32e80f8f930c47d
Shunichi Shinohara <shino@shiguredo.jp>
2019-04-04 15:43:13 +0900

    文字列 JSON をもらうテスト、1.8.0 までのインターフェース

--- sora-android-sdk/src/test/kotlin/jp/shiguredo/sora/sdk/ConnectMetadataJsonTest.kt
+++ sora-android-sdk/src/test/kotlin/jp/shiguredo/sora/sdk/ConnectMetadataJsonTest.kt
@@ -17,6 +17,20 @@ class ConnectMetadataJsonTest {
         assertEquals(message.metadata, null)
     }
 
+    @Test
+    fun stringifiedMetadata() {
+        // sora-android-sdk 1.8.0 までは String? で受けていた。
+        // JSON 文字列をもらったら JSON 文字列に戻る必要がある。
+        val message1 = roundtrip("str")
+        assertEquals(message1.metadata, "str")
+
+        val message2 = roundtrip("{\"foo\": 1, \"bar\": \"baz\"}")
+        assertEquals(message2.metadata, "{\"foo\": 1, \"bar\": \"baz\"}")
+
+        val message3 = roundtrip("[1, 2, 3, \"DAAAAAAAA!!!!\"]")
+        assertEquals(message3.metadata, "[1, 2, 3, \"DAAAAAAAA!!!!\"]")
+    }
+
     private fun roundtrip(metadata: String?) : ConnectMessage {
         val original = ConnectMessage(
                 role = "upstream",
