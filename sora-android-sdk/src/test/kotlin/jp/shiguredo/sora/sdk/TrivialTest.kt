1ae6a0254bbd8bb4e90efb0f03de8f9b775c93af
Shunichi Shinohara <shino@shiguredo.jp>
2019-04-04 14:40:56 +0900

    unit test 環境を追加する

---
+++ sora-android-sdk/src/test/kotlin/jp/shiguredo/sora/sdk/TrivialTest.kt
@@ -0,0 +1,15 @@
+import org.junit.Test
+import kotlin.test.assertTrue
+
+
+class TrivialTest {
+    @Test
+    fun myFirstTest() {
+        assertTrue { true }
+    }
+
+    @Test
+    fun myFirstFailingTest() {
+        assertTrue { false }
+    }
+}
