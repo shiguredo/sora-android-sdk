67e31d1185cb03161532c6ccca977f72b008381b
Shunichi Shinohara <shino@shiguredo.jp>
2019-07-23 11:56:40 +0900

    音声入力周りのオプション項目を追加する

--- sora-android-sdk/src/main/kotlin/jp/shiguredo/sora/sdk/channel/option/SoraAudioOption.kt
+++ sora-android-sdk/src/main/kotlin/jp/shiguredo/sora/sdk/channel/option/SoraAudioOption.kt
@@ -1,10 +1,18 @@
 package jp.shiguredo.sora.sdk.channel.option
 
+import org.webrtc.MediaConstraints
+
 /**
  * 音声に関するオプションをまとめるクラスです
  */
 class SoraAudioOption {
 
+    companion object {
+        const val ECHO_CANCELLATION_CONSTRAINT = "googEchoCancellation"
+        const val AUTO_GAIN_CONTROL_CONSTRAINT = "googAutoGainControl"
+        const val HIGH_PASS_FILTER_CONSTRAINT  = "googHighpassFilter"
+        const val NOISE_SUPPRESSION_CONSTRAINT = "googNoiseSuppression"
+    }
     /**
      * 利用できる音声コーデックを示します。
      */
@@ -14,4 +22,37 @@ class SoraAudioOption {
         /** PCMU */
         PCMU
     }
+
+    /**
+     * 端末組み込みの acoustic echo canceler を使うかどうかの設定
+     *
+     * cf. `org.webrtc.JavaAudioDeviceModule.Builder#setUseHardwareAcousticEchoCanceler()`
+     */
+    var useHardwareAcousticEchoCanceler = true
+
+    /**
+     * 端末組み込みの noise suppressor を使うかどうかの設定
+     *
+     * cf. `org.webrtc.JavaAudioDeviceModule.Builder#setUseHardwareNoiseSuppressor()`
+     */
+    var useHardwareNoiseSuppressor = true
+
+    /**
+     * 音声入力の処理を行うかどうかの設定
+     *
+     * false に設定すると音声の `org.webrtc.MediaConstraints` に以下の設定を追加します。
+     * - `googEchoCancellation` : false
+     * - `googAutoGainControl` : false
+     * - `googHighpassFilter` : false
+     * - `googNoiseSuppression` : false
+     */
+    var audioProcessing = true
+
+    /**
+     * 音声の `org.webrtc.MediaConstraints` を設定します。
+     *
+     * null でない場合、 [audioProcessing] 設定は無視されます。
+     */
+    var mediaConstraints: MediaConstraints? = null
+
 }
