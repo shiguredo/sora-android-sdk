ddf55cfd03b1f2fd6a04e2c9fca10da11daf7f4a
SUZUKI Tetsuya <szktty@shiguredo.jp>
2020-06-04 01:11:47 +0900

    impl

--- sora-android-sdk/src/main/kotlin/jp/shiguredo/sora/sdk/Sora.kt
+++ sora-android-sdk/src/main/kotlin/jp/shiguredo/sora/sdk/Sora.kt
@@ -1,16 +1,26 @@
 package jp.shiguredo.sora.sdk
 
+import android.content.Context
 import jp.shiguredo.sora.sdk.channel.Configuration
 import jp.shiguredo.sora.sdk.channel.MediaChannel
+import org.webrtc.PeerConnection
 import java.lang.Error
 
 class Sora {
 
     companion object {
 
-        fun connect(configuration: Configuration,
+        fun connect(context: Context,
+                    configuration: Configuration,
                     completionHandler: (Result<MediaChannel>) -> Unit) {
-
+            val mediaChannel = MediaChannel(context, configuration)
+            mediaChannel.connect() { error ->
+                if (error == null) {
+                    completionHandler(Result.failure(error!!))
+                } else {
+                    completionHandler(Result.success(mediaChannel))
+                }
+            }
         }
 
     }
