a9bd1156f0731083d73ba9728ffd9bdbfc31745b
SUZUKI Tetsuya <szktty@shiguredo.jp>
2020-06-04 01:11:47 +0900

    impl

--- sora-android-sdk/src/main/kotlin/jp/shiguredo/sora/sdk/channel/MediaChannel.kt
+++ sora-android-sdk/src/main/kotlin/jp/shiguredo/sora/sdk/channel/MediaChannel.kt
@@ -1,22 +1,36 @@
 package jp.shiguredo.sora.sdk.channel
 
 import android.content.Context
+import jp.shiguredo.sora.sdk.channel.signaling.SignalingChannel
 import org.webrtc.MediaStream
+import org.webrtc.PeerConnection
 
-class MediaChannel @JvmOverloads constructor(
+class MediaChannel @JvmOverloads internal constructor(
         private val context: Context,
         val configuration: Configuration
 ) {
 
-    companion object {
-        const val DEFAULT_TIMEOUT_SECONDS = 10L
+    enum class State {
+        READY,
+        CONNECTING,
+        CONNECTED,
+        DISCONNECTING,
+        DISCONNECTED
     }
 
-    private var onConnect: ((Throwable?) -> Unit)? = null
+    var peerConnection: PeerConnection? = null
+    var signalingChannel: SignalingChannel? = null
+    var state: State = State.READY
 
+    private var onConnect: ((Throwable?) -> Unit)? = null
     private var onAddLocalStreamHandler: (stream: MediaStream) -> Unit = {}
     private var onAddRemoteStreamHandler: (stream: MediaStream) -> Unit = {}
     private var onRemoveRemoteStreamHandler: (label: String) -> Unit = {}
+
+    internal fun connect(completionHandler: (Throwable?) -> Unit) {
+        onConnect = completionHandler
+
+    }
 
     fun onAddLocalStream(handler: (stream: MediaStream) -> Unit) {
         onAddLocalStreamHandler = handler
@@ -28,12 +42,6 @@ class MediaChannel @JvmOverloads constructor(
 
     fun onRemoveRemoteStream(handler: (label: String) -> Unit) {
         onRemoveRemoteStreamHandler = handler
-    }
-
-    internal fun connect(timeout: Long = DEFAULT_TIMEOUT_SECONDS,
-                         completionHandler: (Throwable?) -> Unit) {
-        onConnect = completionHandler
-
     }
 
 }
