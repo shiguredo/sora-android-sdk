adca81f47effb4f43fbf5176bea581305d2bea6e
SUZUKI Tetsuya <szktty@shiguredo.jp>
2020-06-06 23:50:17 +0900

    impl

--- sora-android-sdk/src/main/kotlin/jp/shiguredo/sora/sdk/ng/MediaStream.kt
+++ sora-android-sdk/src/main/kotlin/jp/shiguredo/sora/sdk/ng/MediaStream.kt
@@ -1,4 +1,32 @@
 package jp.shiguredo.sora.sdk.ng
 
-class MediaStream {
+import org.webrtc.EglBase
+import org.webrtc.GlRectDrawer
+import org.webrtc.RendererCommon.GlDrawer
+import org.webrtc.RendererCommon.RendererEvents
+
+class MediaStream internal constructor(val mediaChannel: MediaChannel) {
+
+    var nativeVideoTrack: MediaStreamTrack? = null
+    var nativeAudioTrack: MediaStreamTrack? = null
+
+    // video capturer
+    private var videoRenderer: VideoRenderer? = null
+
+    fun setVideoRenderer(newRenderer: VideoRenderer?,
+                         rendererEvents: RendererEvents? = null,
+                         configAttributes: IntArray? = null,
+                         drawer: GlDrawer? = null) {
+        if (newRenderer == null) {
+            videoRenderer = null
+            return
+        }
+
+        newRenderer!!.init(mediaChannel.configuration.eglBase.eglBaseContext,
+                rendererEvents, configAttributes, drawer)
+    }
+
+    internal fun close() {
+    }
+
 }
--- sora-android-sdk/src/main/kotlin/jp/shiguredo/sora/sdk/ng/VideoView.kt
+++ sora-android-sdk/src/main/kotlin/jp/shiguredo/sora/sdk/ng/VideoView.kt
@@ -1,12 +1,48 @@
 package jp.shiguredo.sora.sdk.ng
 
+import android.content.Context
+import android.util.AttributeSet
 import android.view.SurfaceView
+import org.webrtc.EglBase
+import org.webrtc.GlRectDrawer
+import org.webrtc.RendererCommon.GlDrawer
+import org.webrtc.RendererCommon.RendererEvents
 import org.webrtc.SurfaceViewRenderer
+import org.webrtc.VideoFrame
 
-class VideoView: SurfaceView, VideoRenderer {
+class VideoView @JvmOverloads constructor(
+        context: Context,
+        attrs: AttributeSet? = null,
+        defStyleAttr: Int = 0) :
+        SurfaceView(context, attrs, defStyleAttr), VideoRenderer {
 
-    var nativeViewRenderer: SurfaceViewRenderer?
+    var nativeViewRenderer: SurfaceViewRenderer = SurfaceViewRenderer(context, attrs)
 
     var isMirrored: Boolean = false
+
+    override fun init(sharedContext: EglBase.Context,
+                      rendererEvents: RendererEvents?,
+                      configAttributes: IntArray?,
+                      drawer: GlDrawer?) {
+        var configAttributes = configAttributes
+        if (configAttributes == null) {
+            configAttributes = EglBase.CONFIG_PLAIN
+        }
+
+        var drawer = drawer
+        if (drawer == null) {
+            drawer = GlRectDrawer()
+        }
+
+        nativeViewRenderer.init(sharedContext, rendererEvents, configAttributes, drawer)
+    }
+
+    override fun release() {
+        nativeViewRenderer.release()
+    }
+
+    override fun onFrame(frame: VideoFrame) {
+        nativeViewRenderer.onFrame(frame)
+    }
 
 }
