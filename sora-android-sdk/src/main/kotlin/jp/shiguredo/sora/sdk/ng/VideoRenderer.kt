211f20a6dc848308967c45084b5b49ba5666a38f
SUZUKI Tetsuya <szktty@shiguredo.jp>
2020-06-06 23:50:17 +0900

    impl

--- sora-android-sdk/src/main/kotlin/jp/shiguredo/sora/sdk/ng/VideoRenderer.kt
+++ sora-android-sdk/src/main/kotlin/jp/shiguredo/sora/sdk/ng/VideoRenderer.kt
@@ -1,8 +1,18 @@
 package jp.shiguredo.sora.sdk.ng
 
+import org.webrtc.EglBase
+import org.webrtc.RendererCommon.GlDrawer
+import org.webrtc.RendererCommon.RendererEvents
 import org.webrtc.VideoFrame
 
 interface VideoRenderer {
+
+    fun init(sharedContext: EglBase.Context,
+             rendererEvents: RendererEvents?,
+             configAttributes: IntArray?,
+             drawer: GlDrawer?)
+
+    fun release()
 
     fun onFrame(frame: VideoFrame)
 
