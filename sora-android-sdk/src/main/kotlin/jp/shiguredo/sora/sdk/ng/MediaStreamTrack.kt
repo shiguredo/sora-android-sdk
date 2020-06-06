7a1b0244d3cc67ddd100e8cb7c4e10a6caf17a81
SUZUKI Tetsuya <szktty@shiguredo.jp>
2020-06-07 01:34:28 +0900

    impl

--- sora-android-sdk/src/main/kotlin/jp/shiguredo/sora/sdk/ng/MediaStreamTrack.kt
+++ sora-android-sdk/src/main/kotlin/jp/shiguredo/sora/sdk/ng/MediaStreamTrack.kt
@@ -5,7 +5,7 @@ import org.webrtc.RtpReceiver
 import org.webrtc.RtpSender
 import org.webrtc.RtpTransceiver
 
-class MediaStreamTrack internal constructor(var kind: Kind) {
+abstract class MediaStreamTrack {
 
     enum class Kind {
         VIDEO,
@@ -17,26 +17,15 @@ class MediaStreamTrack internal constructor(var kind: Kind) {
         ENDED
     }
 
+    abstract var kind: Kind
+
     var state: State = State.LIVE
         internal set
 
     var stream: MediaStream? = null
         internal set
 
-    var isEnabled: Boolean
-        get() {
-            return if (nativeTrack != null) {
-                nativeTrack!!.enabled()
-            } else {
-                true
-            }
-        }
-        set(value) {
-            nativeTrack?.setEnabled(value)
-        }
-
-    var nativeTrack: org.webrtc.MediaStreamTrack? = null
-        internal set
+    abstract var isEnabled: Boolean
 
     var nativeSender: RtpSender? = null
         internal set
@@ -46,5 +35,7 @@ class MediaStreamTrack internal constructor(var kind: Kind) {
 
     var nativeTransiever: RtpTransceiver? = null
         internal set
+
+    internal abstract fun close()
 
 }
