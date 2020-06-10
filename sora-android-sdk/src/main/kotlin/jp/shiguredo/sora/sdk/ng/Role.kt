bad07df4f450cfa4aa00b96d7ea46f5adcb6f601
SUZUKI Tetsuya <szktty@shiguredo.jp>
2020-06-11 00:56:00 +0900

    impl

---
+++ sora-android-sdk/src/main/kotlin/jp/shiguredo/sora/sdk/ng/Role.kt
@@ -0,0 +1,29 @@
+package jp.shiguredo.sora.sdk.ng
+
+import jp.shiguredo.sora.sdk.channel.option.SoraChannelRole
+
+enum class Role {
+    SENDONLY,
+    RECVONLY,
+    SENDRECV;
+
+    val isSender: Boolean
+        get() = when (this) {
+            SENDONLY, SENDRECV -> true
+            RECVONLY -> false
+        }
+
+    val isReceiver: Boolean
+        get() = when (this) {
+            RECVONLY, SENDRECV -> true
+            SENDONLY -> false
+        }
+
+    val basicRole: SoraChannelRole
+        get() = when (this) {
+            SENDONLY -> SoraChannelRole.UPSTREAM
+            RECVONLY -> SoraChannelRole.DOWNSTREAM
+            SENDRECV -> SoraChannelRole.UPSTREAM
+        }
+
+}
