02cbe0453f1b5c88043d295acd3a66cf5e168018
crimsonwoods <crimsonwoods.dev@gmail.com>
2020-08-09 15:58:52 +0900

    SoraLoggerはobject classとして定義してしまって大丈夫です

--- sora-android-sdk/src/main/kotlin/jp/shiguredo/sora/sdk/util/SoraLogger.kt
+++ sora-android-sdk/src/main/kotlin/jp/shiguredo/sora/sdk/util/SoraLogger.kt
@@ -8,41 +8,37 @@ import android.util.Log
  * cf.
  * - `android.util.Log`
  */
-class SoraLogger {
+object SoraLogger {
+    var enabled = false
+    var libjingle_enabled = false
 
-    companion object {
-        var enabled = false
-        var libjingle_enabled = false
-
-        fun v(tag: String?, msg: String) {
-            if (enabled) {
-                Log.v(tag, msg)
-            }
+    fun v(tag: String?, msg: String) {
+        if (enabled) {
+            Log.v(tag, msg)
         }
+    }
 
-        fun d(tag: String?, msg: String) {
-            if (enabled) {
-                Log.d(tag, msg)
-            }
+    fun d(tag: String?, msg: String) {
+        if (enabled) {
+            Log.d(tag, msg)
         }
+    }
 
-        fun i(tag: String?, msg: String) {
-            if (enabled) {
-                Log.i(tag, msg)
-            }
+    fun i(tag: String?, msg: String) {
+        if (enabled) {
+            Log.i(tag, msg)
         }
+    }
 
-        fun w(tag: String?, msg: String) {
-            if (enabled) {
-                Log.w(tag, msg)
-            }
+    fun w(tag: String?, msg: String) {
+        if (enabled) {
+            Log.w(tag, msg)
         }
+    }
 
-        fun e(tag: String?, msg: String) {
-            if (enabled) {
-                Log.e(tag, msg)
-            }
+    fun e(tag: String?, msg: String) {
+        if (enabled) {
+            Log.e(tag, msg)
         }
     }
 }
-
