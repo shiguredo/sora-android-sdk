613a2f02a2c4a17ee80883e731ee725391907c21
SUZUKI Tetsuya <szktty@shiguredo.jp>
2020-08-31 17:00:48 +0900

    ドキュメントを書く

--- sora-android-sdk/src/main/kotlin/jp/shiguredo/sora/sdk/ng/Configuration.kt
+++ sora-android-sdk/src/main/kotlin/jp/shiguredo/sora/sdk/ng/Configuration.kt
@@ -13,6 +13,15 @@ import org.webrtc.VideoDecoderFactory
 import org.webrtc.VideoEncoderFactory
 import org.webrtc.audio.AudioDeviceModule
 
+/**
+ * サーバーへの接続設定です。
+ *
+ * @property context コンテキスト
+ * @property url サーバーの URL
+ * @property channelId チャネル ID
+ * @property role ロール
+ * @constructor オブジェクトを生成します。
+ */
 class Configuration(var context: Context,
                     var url: String,
                     var channelId: String,
@@ -20,21 +29,28 @@ class Configuration(var context: Context,
 
     companion object {
         private val TAG = Configuration::class.simpleName!!
+
+        /**
+         * デフォルトのタイムアウト秒
+         */
         const val DEFAULT_TIMEOUT_SECONDS = 10L
     }
 
     /**
-     * 接続試行時にタイムアウトするまでの秒数
+     * 接続試行時にタイムアウトするまでの秒数。
+     * デフォルトは 10 秒です。
      */
     var timeout: Long = DEFAULT_TIMEOUT_SECONDS
 
     /**
-     * 映像の可否
+     * 映像の可否。
+     * デフォルトは `true` です。
      */
     var videoEnabled = true
 
     /**
-     * 映像コーデック
+     * 映像コーデック。
+     * デフォルトは `VP9` です。
      */
     var videoCodec: VideoCodec = VideoCodec.VP9
 
@@ -43,27 +59,31 @@ class Configuration(var context: Context,
      */
     var videoBitRate: Int? = null
 
+    /**
+     * 映像キャプチャー
+     */
     var videoCapturer: VideoCapturer? = null
 
     /**
-     * 映像フレームのサイズ
+     * 映像フレームのサイズ。
+     * デフォルトは [VideoFrameSize.VGA] です。
      */
     var videoFrameSize = VideoFrameSize.VGA
 
     /**
-     * 映像のフレームレート
+     * 映像のフレームレート。
+     * デフォルトは 30 です。
      */
     var videoFps: Int = 30
 
     /**
-     * 映像キャプチャーと映像レンダラーで共有して使われる映像描画コンテキスト
+     * 映像キャプチャーと映像レンダラーで共有する映像描画コンテキスト
      */
     var sharedVideoRenderingContext = VideoRenderingContext()
 
     /**
-     * 映像キャプチャーで使われる映像描画コンテキスト
-     * null をセットした場合、 sharedVideoRenderingContext が使われる
-     * デフォルトは null
+     * 映像キャプチャーで使う映像描画コンテキスト。
+     * `null` を指定した場合は [sharedVideoRenderingContext] が使われます。
      */
     var videoCapturerVideoRenderingContext: VideoRenderingContext? = null
 
@@ -71,50 +91,87 @@ class Configuration(var context: Context,
         get() = videoCapturerVideoRenderingContext ?: sharedVideoRenderingContext
 
     /**
-     * 利用する VideoEncoderFactory を指定します
+     * 使用する映像エンコーダー ([org.webrtc.VideoEncoderFactory])
+     *
+     * @see org.webrtc.VideoEncoderFactory
      */
     var videoEncoderFactory: VideoEncoderFactory? = null
 
     /**
-     * 利用する VideoDecoderFactory を指定します
+     * 使用する映像デコーダー ([org.webrtc.VideoDecoderFactory])
+     *
+     * @see org.webrtc.VideoDecoderFactory
      */
     var videoDecoderFactory: VideoDecoderFactory? = null
 
-    // true のとき、 MediaChannel を close すると video renderer も自動的に release する
-    // 接続中の renderer のみ対象とする
+    /**
+     * 映像レンダラー ([VideoRenderer]) のライフサイクルの管理の可否。
+     * この値が `true` の場合、接続終了時に映像レンダラーの終了処理を行います。
+     * 映像レンダラーのライフサイクルを制御したい場合は `false` を指定して下さい。
+     * デフォルトは `true` です。
+     *
+     * @see VideoRenderer
+     */
     var videoRendererLifecycleManagementEnabled: Boolean = true
 
+    /**
+     * 音声の可否。
+     * デフォルトは `true` です。
+     */
     var audioEnabled = true
+
+    /**
+     * 音声コーデック。
+     * デフォルトは `OPUS` です。
+     */
     var audioCodec: AudioCodec = AudioCodec.OPUS
+
+    /**
+     * 音声ビットレート
+     */
     var audioBitRate: Int? = null
 
+    /**
+     * マルチストリームの可否
+     */
     var multistreamEnabled     = false
+
+    /**
+     * サイマルキャストの可否
+     */
     var simulcastEnabled       = false
+
+    /**
+     * スポットライトの可否
+     */
     var spotlightEnabled       = false
 
     var spotlight: Int? = null
 
-    // gson.toJson で扱える型。 Object?
+    /**
+     * シグナリング  `connect` に含めるメタデータ。
+     * この値は JSON に変換されて `metadata` メンバーにセットされます。
+     */
     var signalingMetadata: Any? = null
+
+    /**
+     * シグナリング `connect` に含めるメタデータ。
+     * この値は JSON に変換されて `signaling_notify_metadata` メンバーにセットされます。
+     */
     var signalingNotifyMetadata: Any? = null
 
     /**
-     * 端末組み込みの acoustic echo canceler を使うかどうかの設定
-     *
-     * cf. `org.webrtc.JavaAudioDeviceModule.Builder#setUseHardwareAcousticEchoCanceler()`
+     * 端末の音響エコーキャンセラーの使用の可否
      */
     var usesHardwareAcousticEchoCanceler: Boolean = true
 
     /**
-     * 端末組み込みの noise suppressor を使うかどうかの設定
-     *
-     * cf. `org.webrtc.JavaAudioDeviceModule.Builder#setUseHardwareNoiseSuppressor()`
+     * 端末のノイズサプレッサーの使用の可否
      */
     var usesHardwareNoiseSuppressor: Boolean = true
 
     /**
-     * 利用する AudioDeviceModule の設定
-     *
+     * 使用する音声デバイスモジュール。
      * このプロパティを設定した場合、以下の設定は無視されます。
      *
      * - [usesHardwareAcousticEchoCanceler]
@@ -125,28 +182,27 @@ class Configuration(var context: Context,
     var audioDeviceModule: AudioDeviceModule? = null
 
     /**
-     * 入力音声のエコーキャンセル処理の有無の設定
+     * 入力音声のエコーキャンセル処理の可否
      */
     var audioProcessingEchoCancellationEnabled: Boolean = true
 
     /**
-     * 入力音声の自動ゲイン調整処理の有無の設定
+     * 入力音声の自動ゲイン調整処理の可否
      */
     var audioProcessingAutoGainControlEnabled: Boolean = true
 
     /**
-     * 入力音声のハイパスフィルタ処理の有無の設定
+     * 入力音声のハイパスフィルタ処理の可否
      */
     var audioProcessingHighpassFilterEnabled: Boolean = true
 
     /**
-     * 入力音声のノイズ抑制処理の有無の設定
+     * 入力音声のノイズ抑制処理の可否
      */
     var audioProcessingNoiseSuppressionEnabled: Boolean = true
 
     /**
-     * 音声の `MediaConstraints` の設定
-     *
+     * 音声の制約。
      * このプロパティを設定した場合、以下の設定は無視されます。
      *
      * - [audioProcessingEchoCancellationEnabled]
@@ -157,31 +213,27 @@ class Configuration(var context: Context,
     var audioMediaConstraints: MediaConstraints? = null
 
     /**
-     * 音声ソースの指定
+     * メディアレコーダーが使用する音声ソース ([android.media.MediaRecorder.AudioSource]) 。
+     * デフォルト値は [android.media.MediaRecorder.AudioSource.VOICE_COMMUNICATION] です。
      *
-     * AudioDeviceModule 生成時に利用されます。
-     * デフォルト値は `android.media.MediaRecorder.AudioSource.VOICE_COMMUNICATION` です。
+     * @see android.media.MediaRecorder.AudioSource
      */
     var audioSource: Int = MediaRecorder.AudioSource.VOICE_COMMUNICATION
 
     /**
-     * 入力をステレオにするかどうかのフラグ
-     *
-     * AudioDeviceModule の生成時に利用されます。
+     * 入力音声のモノラルまたはステレオの設定。
      * デフォルト値はモノラルです。
      */
     var inputAudioSound: AudioSound = AudioSound.MONO
 
     /**
-     * 出力をステレオにするかどうかのフラグ
-     *
-     * AudioDeviceModule の生成時に利用されます。
+     * 出力音声のモノラルまたはステレオの設定。
      * デフォルト値はモノラルです。
      */
     var outputAudioSound: AudioSound = AudioSound.MONO
 
     /**
-     * opus_params
+     * OPUS の設定
      */
     var opusParams: OpusParams? = null
 
