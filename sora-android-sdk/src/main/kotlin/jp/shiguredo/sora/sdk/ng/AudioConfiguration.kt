b05206b5505e491c4e056431987be1462c55001b
SUZUKI Tetsuya <szktty@shiguredo.jp>
2020-09-01 15:35:39 +0900

    未使用なので消す

--- sora-android-sdk/src/main/kotlin/jp/shiguredo/sora/sdk/ng/AudioConfiguration.kt
+++ sora-android-sdk/src/main/kotlin/jp/shiguredo/sora/sdk/ng/AudioConfiguration.kt
@@ -15,14 +15,3 @@ enum class AudioSound {
     STEREO,
     MONO,
 }
-
-class AudioConstraint {
-
-    companion object {
-        const val ECHO_CANCELLATION = "googEchoCancellation"
-        const val AUTO_GAIN_CONTROL = "googAutoGainControl"
-        const val HIGH_PASS_FILTER = "googHighpassFilter"
-        const val NOISE_SUPPRESSION = "googNoiseSuppression"
-    }
-
-}
