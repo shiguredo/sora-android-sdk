113b7ca5b8f66602809417c723fc56c4a30a1d0c
enm10k <enm10k@gmail.com>
2022-04-28 17:43:28 +0900

    VideoEncoderHelper を HardwareEncoderWrapper の内部クラスにして CropSizeCalculator にリネームする

--- sora-android-sdk/src/main/kotlin/jp/shiguredo/sora/sdk/codec/HardwareVideoEncoderWrapperFactory.kt
+++ sora-android-sdk/src/main/kotlin/jp/shiguredo/sora/sdk/codec/HardwareVideoEncoderWrapperFactory.kt
@@ -8,108 +8,107 @@ import org.webrtc.VideoEncoder
 import org.webrtc.VideoEncoderFactory
 import org.webrtc.VideoFrame
 
-const val RESOLUTION_ALIGNMENT = 16
-
-class VideoEncoderHelper(private val originalSettings: VideoEncoder.Settings) {
-
-    companion object {
-        val TAG = VideoEncoderHelper::class.simpleName
-    }
-
-    private var adjustedSettings: VideoEncoder.Settings? = null
-
-    val isCropRequired: Boolean
-        get() = adjustedSettings != null
-
-    val settings: VideoEncoder.Settings
-        get() {
-            return if (isCropRequired) {
-                adjustedSettings!!
-            } else {
-                originalSettings
-            }
-        }
-
-    val width: Int
-        get() = settings.width
-
-    val height: Int
-        get() = settings.height
-
-    var cropX = 0
-        private set
-    var cropY = 0
-        private set
-
-    // 最新のフレームのサイズ
-    private var lastFrameWidth: Int = 0
-    private var lastFrameHeight: Int = 0
-
-    init {
-        SoraLogger.i(TAG, "$this init")
-
-        lastFrameWidth = originalSettings.width
-        lastFrameHeight = originalSettings.height
-
-        calculate(originalSettings.width, originalSettings.height)
-    }
-
-    private fun calculate(width: Int, height: Int) {
-        cropX = width % RESOLUTION_ALIGNMENT
-        cropY = height % RESOLUTION_ALIGNMENT
-
-        if (cropX != 0 || cropY != 0) {
-            SoraLogger.i(
-                TAG,
-                "calculate: ${width}x$height => " +
-                    "${width - cropX}x${height - cropY}"
-            )
-            adjustedSettings = VideoEncoder.Settings(
-                originalSettings.numberOfCores,
-                width - cropX,
-                height - cropY,
-                originalSettings.startBitrate,
-                originalSettings.maxFramerate,
-                originalSettings.numberOfSimulcastStreams,
-                originalSettings.automaticResizeOn,
-                originalSettings.capabilities,
-            )
-        }
-    }
-
-    // HardwareVideoEncoder の encode に、フレームのサイズが途中で変化することを考慮した条件があったため実装した関数
-    // 参照: https://source.chromium.org/chromium/chromium/src/+/master:third_party/webrtc/sdk/android/src/java/org/webrtc/HardwareVideoEncoder.java;l=353-362;drc=5a79d28eba61aea39558a492fb4c0ff4fef427ba
-    //
-    // 動作確認をした限り、ネットワーク帯域などに起因してダウンサイズが発生した場合は initEncode が呼ばれており、
-    // この関数は実行されなかったがの念の為に残しておく
-    //
-    // この関数を削除する場合は、 VideoEncoderHelper と HardwareVideoEncoderWrapper の統合を検討すべき
-    fun recalculateIfNeeded(frame: VideoFrame) {
-        if (frame.buffer.width != lastFrameWidth || frame.buffer.height != lastFrameHeight) {
-            SoraLogger.i(
-                TAG,
-                "frame size changed: ${lastFrameWidth}x$lastFrameHeight => ${frame.buffer.width}x${frame.buffer.height}"
-            )
-            lastFrameWidth = frame.buffer.width
-            lastFrameHeight = frame.buffer.height
-
-            calculate(frame.buffer.width, frame.buffer.height)
-        }
-    }
-}
-
 internal class HardwareVideoEncoderWrapper(
     private val encoder: VideoEncoder,
 ) : VideoEncoder {
+    class CropSizeCalculator(private val originalSettings: VideoEncoder.Settings) {
+
+        companion object {
+            val TAG = CropSizeCalculator::class.simpleName
+            const val RESOLUTION_ALIGNMENT = 16
+        }
+
+        var croppedSettings: VideoEncoder.Settings? = null
+
+        val isCropRequired: Boolean
+            get() = croppedSettings != null
+
+        val settings: VideoEncoder.Settings
+            get() {
+                return if (isCropRequired) {
+                    croppedSettings!!
+                } else {
+                    originalSettings
+                }
+            }
+
+        val width: Int
+            get() = settings.width
+
+        val height: Int
+            get() = settings.height
+
+        var cropX = 0
+            private set
+        var cropY = 0
+            private set
+
+        // 最新のフレームの大きさ
+        private var lastFrameWidth: Int = 0
+        private var lastFrameHeight: Int = 0
+
+        init {
+            SoraLogger.i(TAG, "$this init")
+
+            lastFrameWidth = originalSettings.width
+            lastFrameHeight = originalSettings.height
+
+            calculate(originalSettings.width, originalSettings.height)
+        }
+
+        private fun calculate(width: Int, height: Int) {
+            cropX = width % RESOLUTION_ALIGNMENT
+            cropY = height % RESOLUTION_ALIGNMENT
+
+            if (cropX != 0 || cropY != 0) {
+                SoraLogger.i(
+                    TAG,
+                    "calculate: ${width}x$height => " +
+                        "${width - cropX}x${height - cropY}"
+                )
+                croppedSettings = VideoEncoder.Settings(
+                    originalSettings.numberOfCores,
+                    width - cropX,
+                    height - cropY,
+                    originalSettings.startBitrate,
+                    originalSettings.maxFramerate,
+                    originalSettings.numberOfSimulcastStreams,
+                    originalSettings.automaticResizeOn,
+                    originalSettings.capabilities,
+                )
+            }
+        }
+
+        // HardwareVideoEncoder の encode に、フレームのサイズが途中で変化することを考慮した条件があったため実装した関数
+        // 参照: https://source.chromium.org/chromium/chromium/src/+/master:third_party/webrtc/sdk/android/src/java/org/webrtc/HardwareVideoEncoder.java;l=353-362;drc=5a79d28eba61aea39558a492fb4c0ff4fef427ba
+        //
+        // 動作確認をした限り、ネットワーク帯域などに起因してダウンサイズが発生した場合は initEncode が呼ばれており、
+        // この関数は実行されなかったが念の為に残しておく
+        //
+        // この関数を削除する場合は HardwareVideoEncoderWrapper との統合を検討すべき
+        fun recalculateIfNeeded(frame: VideoFrame) {
+            if (frame.buffer.width != lastFrameWidth || frame.buffer.height != lastFrameHeight) {
+                SoraLogger.i(
+                    TAG,
+                    "frame size changed: ${lastFrameWidth}x$lastFrameHeight => ${frame.buffer.width}x${frame.buffer.height}"
+                )
+                lastFrameWidth = frame.buffer.width
+                lastFrameHeight = frame.buffer.height
+
+                calculate(frame.buffer.width, frame.buffer.height)
+            }
+        }
+    }
+
     companion object {
         val TAG = HardwareVideoEncoderWrapper::class.simpleName
     }
 
-    private var helper: VideoEncoderHelper? = null
+    private var calculator: CropSizeCalculator? = null
 
     override fun initEncode(settings: VideoEncoder.Settings, callback: VideoEncoder.Callback?): VideoCodecStatus {
-        helper = VideoEncoderHelper(settings)
-        return encoder.initEncode(helper!!.settings, callback)
+        calculator = CropSizeCalculator(settings)
+        return encoder.initEncode(calculator!!.settings, callback)
     }
 
     override fun release(): VideoCodecStatus {
@@ -117,7 +116,7 @@ internal class HardwareVideoEncoderWrapper(
     }
 
     override fun encode(frame: VideoFrame, encodeInfo: VideoEncoder.EncodeInfo?): VideoCodecStatus {
-        return helper?.let {
+        return calculator?.let {
             it.recalculateIfNeeded(frame)
 
             if (!it.isCropRequired) {
