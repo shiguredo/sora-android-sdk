78d8f2bee8a04a02d7fbd9eb9a0ed618ab8f0b00
SUZUKI Tetsuya <tetsuya.suzuki@gmail.com>
2023-04-27 16:24:09 +0900

    転送フィルター機能の設定を追加する

---
+++ sora-android-sdk/src/main/kotlin/jp/shiguredo/sora/sdk/channel/option/SoraForwardingFilterOption.kts
@@ -0,0 +1,59 @@
+/**
+ * 転送フィルター機能の設定を表すクラスです。
+ *
+ * @param action フィルター適用時の挙動
+ * @param rules フィルターの適用ルール
+ */
+class SoraForwardingFilterOption(
+        val action: Action,
+        val rules: List<List<Rule>>
+) {
+    /**
+     * フィルター適用時の挙動を表します。
+     */
+    enum class Action {
+        /** block */
+        BLOCK,
+
+        /** allow */
+        ALLOW
+    }
+
+    /**
+     * フィルターの適用ルールを表します。
+     *
+     * @param field フィルター対象のフィールド
+     * @param operator フィルターの演算子
+     * @param values フィルターの値
+     */
+    class Rule(
+            val field: Field,
+            val operator: Operator,
+            val values: List<String>
+    ) {
+        /**
+         * フィルター対象のフィールドを表します。
+         */
+        enum class Field {
+            /** connection_id */
+            CONNECTION_ID,
+
+            /** client_id */
+            CLIENT_ID,
+
+            /** kind */
+            KIND
+        }
+
+        /**
+         * フィルターの演算子を表します。
+         */
+        enum class Operator {
+            /** is_in */
+            IS_IN,
+
+            /** is_not_in */
+            IS_NOT_IN
+        }
+    }
+}
