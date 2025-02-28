05a8671280530800fe3abee8753b718a30086b59
zztkm <zztkm@tsurutatakumi.info>
2025-02-28 19:03:43 +0900

    コメント追加

--- sora-android-sdk/src/main/kotlin/jp/shiguredo/sora/sdk/channel/signaling/SignalingDisconnectResult.kt
+++ sora-android-sdk/src/main/kotlin/jp/shiguredo/sora/sdk/channel/signaling/SignalingDisconnectResult.kt
@@ -1,6 +1,8 @@
 package jp.shiguredo.sora.sdk.channel.signaling
 
-// TODO(zztkm): プロパティはこれで良いか確認する
+/**
+ * WebSocket シグナリング切断結果
+ */
 data class SignalingDisconnectResult(
     val code: Int,
     val reason: String,
