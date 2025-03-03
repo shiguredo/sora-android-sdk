a95c71ce19261b76afc0fa48513e1d08cf25c503
zztkm <zztkm@tsurutatakumi.info>
2025-03-03 19:17:53 +0900

    package 内のみアクセスできるようにする

--- sora-android-sdk/src/main/kotlin/jp/shiguredo/sora/sdk/channel/signaling/SignalingChannelDisconnectResult.kt
+++ sora-android-sdk/src/main/kotlin/jp/shiguredo/sora/sdk/channel/signaling/SignalingChannelDisconnectResult.kt
@@ -1,6 +1,6 @@
 package jp.shiguredo.sora.sdk.channel.signaling
 
-data class SignalingChannelDisconnectResult(
+internal data class SignalingChannelDisconnectResult(
     val code: Int,
     val reason: String
 )
