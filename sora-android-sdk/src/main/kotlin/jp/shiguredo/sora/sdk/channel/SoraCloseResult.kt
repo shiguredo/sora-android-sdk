3e695b6518679fff431c35a569902b4ade4a6039
zztkm <zztkm@tsurutatakumi.info>
2025-03-12 16:06:50 +0900

    data class の説明を追加する

--- sora-android-sdk/src/main/kotlin/jp/shiguredo/sora/sdk/channel/SoraCloseResult.kt
+++ sora-android-sdk/src/main/kotlin/jp/shiguredo/sora/sdk/channel/SoraCloseResult.kt
@@ -1,5 +1,11 @@
 package jp.shiguredo.sora.sdk.channel
 
+/**
+ * SoraCloseResult は Sora から接続が切断された際の結果を表します。
+ *
+ * @param code 切断時のステータスコード
+ * @param reason 切断理由
+ */
 data class SoraCloseResult(
     val code: Int,
     val reason: String,
