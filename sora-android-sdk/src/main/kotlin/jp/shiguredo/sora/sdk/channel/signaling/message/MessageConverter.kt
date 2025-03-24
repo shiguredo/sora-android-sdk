78116bf9449a633b168f45f419cd2477af3b7d50
zztkm <zztkm@tsurutatakumi.info>
2025-03-24 16:16:59 +0900

    remove unused import

--- sora-android-sdk/src/main/kotlin/jp/shiguredo/sora/sdk/channel/signaling/message/MessageConverter.kt
+++ sora-android-sdk/src/main/kotlin/jp/shiguredo/sora/sdk/channel/signaling/message/MessageConverter.kt
@@ -1,6 +1,5 @@
 package jp.shiguredo.sora.sdk.channel.signaling.message
 
-import android.provider.MediaStore.Video
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import com.google.gson.JsonObject
